438bfeed353f61cc5ff8f16c5f7fa31ecb7a57ca
Wumpie7 <chrisjaver@gmail.com>
2024-11-17 23:41:11 +0100

    Added base Algorithmics classes

---
+++ Assets/Scripts/Algorithmics/L1/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public static List<Square> squares;
+
+    public static int score = 0;
+
+    private void Awake()
+    {
+        squares = new List<Square>();
+    }
+
+    private void Update()
+    {
+        if (squares.Count == 0)
+        {
+            Victory();
+        }
+    }
+
+    public static void Defeat()
+    {
+        score = 0;
+        UI.ShowDefeatPanel();
+    }
+
+    public static void Victory()
+    {
+        UI.ShowVictoryPanel();
+    }
+
+    public static void Restart()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index);
+    }
+
+}
