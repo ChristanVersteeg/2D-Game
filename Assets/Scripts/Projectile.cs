8a7c56a4f5655be4870ea043f02e2106801d38ff
Wumpie7 <chrisjaver@gmail.com>
2024-12-01 21:18:02 +0100

    Created the Tag class and fixed tag checks

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,12 +1,14 @@
+using Tags;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Player"))
+        if (collision.CompareTag(Tag.Obstacle))
         {
             Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Scripts/Tag.cs
@@ -0,0 +1,9 @@
+namespace Tags
+{
+    public static class Tag
+    {
+        public const string Player = nameof(Player);
+        public const string Item = nameof(Item);
+        public const string Obstacle = nameof(Obstacle);
+    }
+}
