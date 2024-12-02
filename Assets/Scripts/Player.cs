878d625e6e18b2bbb65bb69c0c1652658c328f71
Wumpie7 <chrisjaver@gmail.com>
2024-12-02 17:58:53 +0100

    Spawner

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,17 @@ public class Player : MonoBehaviour
 {
     public int points;
 
-    void Update()
+    private void Update()
     {
         Move();
     }
 
     private void Move()
     {
+        Vector2 vector1 = Vector2.one;
+        Vector2 vector2 = Vector2.one * 2;
+        Vector2 vector3 = vector1 / vector2;
+
         if (Input.GetMouseButton(0))
         {
             Vector2 mousePos = Input.mousePosition;
