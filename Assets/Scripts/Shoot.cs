0de6682f6c917d9107d4a70bb75a8f7cb628efff
Marijnvankampen <marijnvankampen2012@gmail.com>
2024-12-09 17:32:14 +0100

    shoot

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,15 +1,42 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
+using Unity.IO.LowLevel.Unsafe;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private Transform bulletsParent;
+    [SerializeField] private float shootdelay;
+    private bool cooldown;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GameObject projectile = Instantiate(projectilePrefab, transform);
-            projectile.transform.parent = bulletsParent;
-        }
+
+            if (!cooldown)
+            {
+                fire();
+                StartCoroutine(nameof(shootDelay));
+
+
+            }
+    }
+
+
+
+    private void fire()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, transform);
+        projectile.transform.parent = bulletsParent;
+    }
+
+
+    private IEnumerator shootDelay()
+
+    {
+        cooldown = true;
+        yield return new WaitForSeconds(shootdelay);
+        cooldown = false;
     }
 }
