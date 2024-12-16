dc899f143c8ce32eda50851094d59f792b786e3e
Jaimielol <jaimieglennlolparty@gmail.com>
2024-12-16 17:38:09 +0100

    spawner items

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 public class Spawner : MonoBehaviour
 
@@ -12,8 +15,6 @@ public class Spawner : MonoBehaviour
     {
         StartCoroutine(nameof(Delay));
     }
-
-
     private IEnumerator Delay()
     {
         while (true)
@@ -22,22 +23,28 @@ public class Spawner : MonoBehaviour
             yield return new WaitForSeconds(1);
         }
     }
+    private GameObject ReturnRandomObject()
+    {
+        int randomObjectIndex = Random.Range(0, 3);
+        GameObject randomObject;
+
+        if (randomObjectIndex <= 1)
+        {
+            randomObject = obstacle;
+        }
+        else
+        {
+            randomObject = item;
+        }
+        return randomObject;
+    }
     private void spawn()
     {
         float randomX = Random.Range(leftborder.position.x, rightborder.position.x);
-        int randomY = Random.Range(0,360);
+        int randomY = Random.Range(0, 360);
         float randomScale = Random.Range(1, 4);
-        GameObject spawnedObject = Instantiate(obstacle, new Vector2(randomX, transform.position.y), Quaternion.Euler(0, 0, randomY));
+       
+        GameObject spawnedObject = Instantiate(ReturnRandomObject(), new Vector2(randomX, transform.position.y), Quaternion.Euler(0, 0, randomY));
         spawnedObject.transform.localScale = Vector3.one * (randomScale / 10);
     }
-
-
-
-     
-
-
-
-
-
-
 }
