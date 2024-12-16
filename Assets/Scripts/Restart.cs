c434cb77086209fbbbe84948daddc630b1dde5c7
Wumpie7 <chrisjaver@gmail.com>
2024-12-16 16:55:53 +0100

    Changed the subscription approach

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -1,12 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Restart : MonoBehaviour
 {
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
     public void RestartScene()
     {
         SceneManager.LoadScene(0);
+    }
+
+    private void OnEnable()
+    {
+        button.onClick.AddListener(RestartScene);
+    }
+
+    private void OnDisable()
+    {
+        button.onClick.RemoveListener(RestartScene);
     }
 }
