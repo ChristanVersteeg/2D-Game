8980d6d85c0375af8d05a439efed19e6daac924b
Wumpie7 <chrisjaver@gmail.com>
2025-01-31 17:56:58 +0100

    Added enemy logic and corn logic

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -13,4 +13,12 @@ public class Arrow : MonoBehaviour
     {
         transform.position += speed * Time.deltaTime * transform.up;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<Enemy>().TakeDamage();
+        }
+    }
 }
