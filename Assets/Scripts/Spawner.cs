7f7d9c6e4f698598929becb85fcf7054e39c8aa9
Wumpie7 <chrisjaver@gmail.com>
2025-02-03 18:05:47 +0100

    Added a spawner system

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEditor.Timeline;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform topBorder, bottomBorder;
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private float minSpawnTime, maxSpawnTime;
+    [SerializeField] private int maxSpawnCount;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private IEnumerator Spawn()
+    {
+        int spawnCount = 0;
+
+        while (spawnCount < maxSpawnCount)
+        {
+            float randSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(randSpawnTime);
+            float randY = Random.Range(bottomBorder.position.y, topBorder.position.y);
+            Instantiate(enemy, new Vector3(transform.position.x, randY, 0), Quaternion.identity);
+            spawnCount++;
+        }
+    }
+}
